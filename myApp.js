0be189d33d51b0e40f8ebd0e48eab7918280afeb
JamesMancuso99 <>
2021-04-18 22:27:36 +0000

    Activity #5

--- myApp.js
+++ myApp.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 
+const mySecret = process.env['MESSAGE_STYLE']
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({"message" : "HELLO JSON"});
+	} else {
+		res.json({"message" : "Hello json"});
+	}
 });
 
 // Normal usage
