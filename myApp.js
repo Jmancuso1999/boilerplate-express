3473c678b89d0c94cd4375f74a8b5bd00c7d995d
James Mancuso <Jmancuso1999@gmail.com>
2021-04-18 16:03:54 -0400

    activity #1

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+app.listen(3000, () => {
+	console.log('App listening on port 3000');
+});
 
 
 
