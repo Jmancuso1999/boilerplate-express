e50e4ed9a65221f2a8595da53ffcea2615396526
JamesMancuso99 <>
2021-04-18 20:13:22 +0000

    Activity #1

--- myApp.js
+++ myApp.js
@@ -1,46 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello World!');
-});
-
-app.listen(3000, () => {
-	console.log('App listening on port 3000');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Hello World!")
 
  module.exports = app;
